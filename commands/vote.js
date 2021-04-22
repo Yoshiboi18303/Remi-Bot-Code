c22c2a25ffc6ded8af3df148612dacea625ce0dc
Brice Coley <yoshiboi18303.t@gmail.com>
2021-04-21 22:15:02 -0400

    Updated commands/vote.js
    Updated the File as there's more to it!

--- commands/vote.js
+++ commands/vote.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
   if(!message.content.startsWith('Robux;'))return;  
 
+   let user = message.author;
+
      let votesEmbed = new Discord.MessageEmbed()
-      .setTitle(`Upvote RobuxBot (me)!`)
+      .setTitle(`Upvote ${bot.user.username}!`)
       .setColor("#5C5C5C")
       .setFooter(`Requested by ${message.author.username} | Made by Yoshiboi_Development`)
-      .setDescription(`__**Coming Soon!**__`)
+      .setDescription(`Hey ${user}, I see you want to vote for me! Here are some links you can give me an Upvote!\n\nFates List: [Click Me!](https://fateslist.xyz/bot/819225480635875359/vote)\nInfinity Bot List: [Click Me!](https://infinitybotlist.com/bots/819225480635875359/vote)\n\n**More __Coming Soon__!**`)
       message.channel.send(votesEmbed)
 
   }
